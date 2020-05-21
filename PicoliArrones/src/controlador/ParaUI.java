f8d5ebf29f6ca6b7347afca04aed8a2764c474d3
markusLateralus <52543660+markusLateralus@users.noreply.github.com>
2020-05-21 13:44:29 +0200

    moverEjercito

--- PicoliArrones/src/controlador/ParaUI.java
+++ PicoliArrones/src/controlador/ParaUI.java
@@ -1,10 +1,6 @@
 package controlador;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Cursor;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -13,7 +9,6 @@ import java.awt.event.MouseMotionAdapter;
 import java.util.LinkedList;
 
 import javax.swing.JButton;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -40,10 +35,7 @@ public class ParaUI extends UserInterface {
 	private MercadoTipo mercadoTipo;
 	private int ancho = 12, alto = 6;
 	MouseAdapter mouseAdapter = new MouseAdapter() {
-		Coordenada coordenadaOrigen;
-		//Coordenada coordenadaDestino;
-		
-	
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			super.mouseClicked(e);
@@ -63,86 +55,29 @@ public class ParaUI extends UserInterface {
 			getTableroUI().actualizarTablero(getTableroUIInfo(controller.getJuego()));
 			
 		}
-
+	
 
 	};
 	MouseAdapter mouseAdapter2 = new MouseAdapter() {
-	       private  int X, Y;
+
 		@Override
 		public void mouseDragged(MouseEvent e) {	
 	
 			JPanel panel = (JPanel) e.getSource();
 			Coordenada	coordenadaDestino = Utiles.getCoordenada(panel.getName());
-
-	        int x = e.getX() + X;
-	        int xMax = getBordeArmada().getWidth() - panel.getWidth();
-	        x = Math.min(x, xMax);
-	        x = Math.max(x, 0);
-
-	        int y = e.getY() + Y;
-	        int yMax = getBordeArmada().getHeight() - panel.getHeight();
-	        y = Math.min(y, yMax);
-	        y = Math.max(y, 0);
-
-	        panel.setLocation(x, y);
-		}	
+			System.out.println("DESTINO " + coordenadaDestino.toString());
+			controller.getJuego().getCasillaMarcada();
+	//	controller.getJuego().poner( coordenadaDestino);
+		getTableroUI().actualizarTablero(getTableroUIInfo(controller.getJuego()));
+			
+		}
+		
 		@Override
 		public void mousePressed(MouseEvent e){
 			JPanel panel = (JPanel) e.getSource();
 			Coordenada	coordenadaOrigen = Utiles.getCoordenada(panel.getName());
-			  Component c =  panel.findComponentAt(e.getX(), e.getY());
-	        Point parentLocation = c.getParent().getLocation();
-	        X= parentLocation.x - e.getX();
-	        Y = parentLocation.y - e.getY();
-	        panel = (JPanel)c;
-	        panel.setLocation(e.getX() + X, e.getY() + Y);
-
-	        getBordeArmada().add(panel, JLayeredPane.DRAG_LAYER);
-	        getBordeArmada().setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
-	System.out.println("PRESIONADO");
-			//controller.getJuego().setCasillaMarcada(coordenadaOrigen);
+			controller.getJuego().poner( coordenadaOrigen);
 		}
-
-		@Override
-		 public void mouseReleased(MouseEvent e)
-		    {
-			JPanel panel = (JPanel) e.getSource();
-			getBordeArmada().setCursor(null);
-
-		        if (panel == null) return;
-
-		        //  Make sure the chess piece is no longer painted on the layered pane
-
-		        panel.setVisible(false);
-		        getBordeArmada().remove(panel);
-		        panel.setVisible(true);
-
-		        //  The drop location should be within the bounds of the chess board
-
-		        int xMax = getBordeArmada().getWidth() - panel.getWidth();
-		        int x = Math.min(e.getX(), xMax);
-		        x = Math.max(x, 0);
-
-		        int yMax = getBordeArmada().getHeight() - panel.getHeight();
-		        int y = Math.min(e.getY(), yMax);
-		        y = Math.max(y, 0);
-
-		        Component c =  panel.findComponentAt(x, y);
-
-		        if (c instanceof JPanel)
-		        {
-		            Container parent = c.getParent();
-		            parent.remove(0);
-		            parent.add( panel );
-		            parent.validate();
-		        }
-		        else
-		        {
-		            Container parent = (Container)c;
-		            parent.add( panel );
-		            parent.validate();
-		        }
-		    }
 	};
 
 //	MouseMotionAdapter mouseMotion=new MouseMotionAdapter() {
@@ -171,7 +106,7 @@ public class ParaUI extends UserInterface {
 		controller = new Controller(ancho, alto);
 		crearTablero(controller);
 		getTableroUI().setMouseAdapter(mouseAdapter);
-		getTableroUI().setMouseAdapter2(mouseAdapter2);
+		//getTableroUI().setMouseAdapter2(mouseAdapter2);
 		getTableroUI().actualizarTablero(getTableroUIInfo(controller.getJuego()));
 		getBordeArmada().cargarEjercito(Generador.getEjercitoInfo(controller.getJuego().getEjercitoActual()));
 		getBtnPoblar().addActionListener(new ActionListener() {
